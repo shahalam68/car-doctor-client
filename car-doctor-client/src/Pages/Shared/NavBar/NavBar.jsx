ec5f44e0f597c5bac90bbb9bbdc5501f10023901
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2023-12-14 13:48:35 +0600

    hero section

--- car-doctor-client/src/Pages/Shared/NavBar/NavBar.jsx
+++ car-doctor-client/src/Pages/Shared/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 h-28">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
