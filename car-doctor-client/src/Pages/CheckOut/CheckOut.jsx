fb31f49d33b93417e109009148869302be71003c
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-02 12:53:18 +0600

    Add button in ser vice section

---
+++ car-doctor-client/src/Pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,9 @@
+const CheckOut = () => {
+    return (
+        <div>
+            <h2>checkout</h2>
+        </div>
+    );
+};
+
+export default CheckOut;
--- car-doctor-client/src/Pages/Home/Services/ServiceCard.jsx
+++ car-doctor-client/src/Pages/Home/Services/ServiceCard.jsx
@@ -1,5 +1,8 @@
-const ServiceCard = ({service}) => {
-    const {_id, title, img,price} = service;
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa6";
+
+const ServiceCard = ({ service }) => {
+  const { _id, title, img, price } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="pt-5">
@@ -11,7 +14,12 @@ const ServiceCard = ({service}) => {
       </figure>
       <div className="card-body  ">
         <h2 className="card-title text-[#444] font-bold">{title}</h2>
-        <p className="text-[#FF3811] text-xl font-semibold">${price}</p>
+        <div className="flex ">
+          <p className="text-[#FF3811] text-xl font-semibold">${price}</p>
+          <Link to={`checkout/${_id}`}>
+            <FaArrowRight className="text-[#FF3811] text-xl font-semibold" />
+          </Link>
+        </div>
       </div>
     </div>
   );
